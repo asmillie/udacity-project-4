2de0cf9b2b883470648dca20de616f67b2c26af6
Alan Smillie <alan.smillie@gmail.com>
2021-02-18 15:21:57 -0500

    Remove redundant interface

--- backend/src/todo/ToDo.ts
+++
@@ -1,9 +0,0 @@
-export interface ToDo {
-    todoId: string;
-    userId: string;
-    createdAt: string;
-    name: string;
-    dueDate: string;
-    done: boolean;
-    attachmentUrl?: string;
-}
