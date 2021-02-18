fc6f82a8482f26ab91861cd86a183cdadd0850db
Alan Smillie <alan.smillie@gmail.com>
2021-02-18 13:04:55 -0500

    Implement token verification

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,22 +1,25 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import { APIGatewayAuthorizerResult, APIGatewayTokenAuthorizerEvent } from 'aws-lambda'
 import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import axios from 'axios'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-x4xgby3m.us.auth0.com/.well-known/jwks.json';
+
+// const secretId = process.env.AUTH_0_SECRET_ID;
+// const secretField = process.env.AUTH_0_SECRET_FIELD;
 
 export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
+  event: APIGatewayTokenAuthorizerEvent
+): Promise<APIGatewayAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
@@ -54,14 +57,30 @@ export const handler = async (
   }
 }
 
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
+async function verifyToken(authHeader: string): Promise<JwtPayload | boolean> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  console.log('Retrieving Signing Certificate from Auth0');
+  let cert = '';
+  axios({
+    method: 'get',
+    responseType: 'json'
+  }).then(res => {
+    cert = res.data
+  }).catch(err => {
+    console.log(`Error retrieving signing certificate from Auth0: ${err}`);
+    return false;
+  });
+
+  if (!cert || cert == '') {
+    return false;
+  }
+  
+  return verify(token, cert, { algorithms: ['RS256']}) as JwtPayload;
 }
 
 function getToken(authHeader: string): string {
