3f894d04f437f1d15cf1717d4ae70aa20f4ace83
Alan Smillie <alan.smillie@gmail.com>
2021-02-22 11:11:53 -0500

    Implement handler logic

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,10 +3,14 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { ToDoRepository } from '../../todo/todo.repository'
+import { prepareApiResponse } from '../utils'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const toDoRepository = new ToDoRepository();
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const result = await toDoRepository.createToDo(newTodo);
+
+  return prepareApiResponse(201, result);
 }
